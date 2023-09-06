684eb1921d4858e97b9799130aea16c25bcb28a1
ionelaSabinaMacovei <>
2023-09-06 16:41:03 +0000

    Added review model + fixed bug in products model

--- products/models.py
+++ products/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+import random
+import string
+from django.utils import timezone
+from django.core.exceptions import ValidationError
+
+from django.contrib.auth.models import User
+from profiles.models import UserProfile
 
 
 class Category(models.Model):
@@ -41,26 +48,26 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    on_sale = models.BooleanField(default=False)
-    sale_price = models.DecimalField(max_digits=4, decimal_places=2)
-    discount = models.IntegerField(
-        default=10,
-        help_text="Discount in Percentage",
-        verbose_name="Discount If Product On Sale",
-    )
-    discounted_price = models.IntegerField(null=True)
+    #on_sale = models.BooleanField(default=False)
+    #sale_price = models.DecimalField(max_digits=4, decimal_places=2)
+    #discount = models.IntegerField(
+    #    default=10,
+    #    help_text="Discount in Percentage",
+     #   verbose_name="Discount If Product On Sale",
+    #)
+    #discounted_price = models.IntegerField(null=True)
 
     def clean(self):
         if self.price < 0:
             raise ValidationError("Price cannot be negative.")
 
-    @property
-    def discounted_price(self):
-        return ((self.price) * (self.discount)) / 100
+    #@property
+    #def discounted_price(self):
+    #    return ((self.price) * (self.discount)) / 100
 
-    @property
-    def sale_price(self):
-        return (self.price) - (self.discounted_price)
+    #@property
+    #def sale_price(self):
+    #    return (self.price) - (self.discounted_price)
 
     def __str__(self):
         return self.name
@@ -82,4 +89,4 @@ class Reviews(models.Model):
         ]
 
     def __str__(self):
-        return self.title
+        return self.name
