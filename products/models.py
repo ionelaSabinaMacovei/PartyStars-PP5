d27921be661dd57f0c5f6c64dd323aecf868e771
ionelaSabinaMacovei <>
2023-09-06 16:16:35 +0000

    Updated the product model

--- products/models.py
+++ products/models.py
@@ -29,13 +29,38 @@ def generate_sku():
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.CharField(
+        max_length=6,
+        unique=True,
+        default=generate_sku,
+        help_text="SKU randomly generated",
+    )
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    on_sale = models.BooleanField(default=False)
+    sale_price = models.DecimalField(max_digits=4, decimal_places=2)
+    discount = models.IntegerField(
+        default=10,
+        help_text="Discount in Percentage",
+        verbose_name="Discount If Product On Sale",
+    )
+    discounted_price = models.IntegerField(null=True)
 
+    def clean(self):
+        if self.price < 0:
+            raise ValidationError("Price cannot be negative.")
+
+    @property
+    def discounted_price(self):
+        return ((self.price) * (self.discount)) / 100
+
+    @property
+    def sale_price(self):
+        return (self.price) - (self.discounted_price)
+        
     def __str__(self):
         return self.name
