f392d4296a8bbc86b48ad4a57e95381d62a8d063
ionelaSabinaMacovei <>
2023-09-08 14:36:05 +0000

    Fixed bugin bag app for grand total

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,7 +22,7 @@ def bag_contents(request):
 
     for item_id, quantity in bag.items():
         product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
+        total += quantity * product.sort_price
         product_count += quantity
         bag_items.append({
             'item_id': item_id,
@@ -30,7 +30,7 @@ def bag_contents(request):
             'product': product,
         })
 
-    if total < settings.FREE_DELIVERY_THRESHOLD:
+    if total != 0 and total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
