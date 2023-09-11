788ec5cda92683f84dc117b41d8ec9f7d699c287
ionelaSabinaMacovei <>
2023-09-11 10:12:01 +0000

    Updated the views ans create new templte for profile section

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('order_history/', views.order_history, name='order_history'),
+    path('', views.account_overview, name='account_overview'),
+    path('order_confirmation/<order_number>',
+         views.order_confirmation, name='order_confirmation'),
+    path('admin_profile/', views.admin_profile, name='admin_profile'),
+    path('product_management/', views.product_management, name='product_management'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
@@ -12,6 +12,7 @@ from checkout.models import Order
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
@@ -22,19 +23,31 @@ def profile(request):
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
         form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
 
     template = 'profiles/profile.html'
     context = {
         'form': form,
-        'orders': orders,
         'on_profile_page': True
     }
 
     return render(request, template, context)
 
 
-def order_history(request, order_number):
+@login_required
+def order_history(request):
+    """ Display order history """
+    profile = get_object_or_404(UserProfile, user=request.user)
+    # get all orders under user name
+    orders = profile.orders.all().order_by('-id')
+    context = {
+        'orders': orders,
+        'on_page': True,
+    }
+    return render(request, 'profiles/order_history.html', context)
+
+
+@login_required
+def order_confirmation(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
@@ -49,3 +62,32 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def account_overview(request):
+    """ Display the account overview"""
+    template = 'profiles/account_overview.html'
+    return render(request, template)
+
+
+@login_required
+def admin_profile(request):
+    """ Display admin account overview"""
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that.')
+        return redirect(reverse('home'))
+
+    template = 'profiles/admin_profile.html'
+    return render(request, template)
+
+
+@login_required
+def product_management(request):
+    """ Display product management page where admin
+    can choose to add category and book """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry only store owners can do that.')
+        return redirect(reverse('home'))
+    template = 'profiles/product_managment.html'
+    return render(request, template)
