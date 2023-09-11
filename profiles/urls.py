5406855ba273c949fd6cebdeb0271e738f747997
ionelaSabinaMacovei <>
2023-09-11 10:40:48 +0000

    Updated profile template

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.profile, name='profile'),
-    path('order_history/', views.order_history, name='order_history'),
     path('', views.account_overview, name='account_overview'),
+    path('profile/', views.profile, name='profile'),
+    path('order_history/', views.order_history, name='order_history'),
+    
     path('order_confirmation/<order_number>',
          views.order_confirmation, name='order_confirmation'),
     path('admin_profile/', views.admin_profile, name='admin_profile'),
