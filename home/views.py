5dbbdc40f1c21cb6cebc2f6a5903ab488a1c96e7
ionelaSabinaMacovei <>
2023-09-22 15:20:22 +0000

    Added carousel for home page

--- home/views.py
+++ home/views.py
@@ -6,4 +6,5 @@ from django.shortcuts import render
 def index(request):
     """ A view to return the index page """
 
-    return render(request, 'home/index.html')
+    template = 'home/index.html'
+    return render(request, template)
