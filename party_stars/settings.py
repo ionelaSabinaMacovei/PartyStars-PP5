7edddf4749ff77accaf929eda2bdcee36394da5b
ionelaSabinaMacovei <>
2023-09-27 16:15:14 +0000

    Added env.py

--- party_stars/settings.py
+++ party_stars/settings.py
@@ -15,6 +15,9 @@ from pathlib import Path
 import os
 import dj_database_url
 
+if os.path.exists('env.py'):
+    import env  # noqa
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,8 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-ionelasabinamacovei-part-33chiqflxx.us2.codeanyapp.com', 'partystars-pp5.herokuapp.com']
 
