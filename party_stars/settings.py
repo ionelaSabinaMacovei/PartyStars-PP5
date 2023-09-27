f0a63bc6abff51b457f43cd70077185b4e86abe0
ionelaSabinaMacovei <>
2023-09-27 15:54:16 +0000

    Removed python ubuntu from requiremets.txt

--- party_stars/settings.py
+++ party_stars/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e2#=#254ylw$uvk&da%njwrfn)kw#coc5k$l556h3@m@ti_(4g'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
