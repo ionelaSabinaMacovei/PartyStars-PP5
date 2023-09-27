ba6cdf019878f206670b93fb48f90ebf450245e3
ionelaSabinaMacovei <>
2023-09-27 16:58:48 +0000

    Update debug

--- party_stars/settings.py
+++ party_stars/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-ionelasabinamacovei-part-33chiqflxx.us2.codeanyapp.com', 'partystars-pp5.herokuapp.com']
 
